59831c900ace839516ad0ebb8e60ad34055c64a4
Khyle Matthew P. Yanes <yaneskhylematthew02@gmail.com>
2024-06-19 19:10:26 +0800

    Adding Header and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className=" h-[50rem]">
         <h1>Hello World!</h1>
       </div>
     </main>
