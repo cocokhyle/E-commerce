939ea8f7d88f9c2956191fe8cf89a1b7eb234346
Khyle Matthew P. Yanes <yaneskhylematthew02@gmail.com>
2024-06-23 16:28:30 +0800

    Added Create UI

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,15 +1,45 @@
+"use client";
+import { useState } from "react";
+
 export default function Contact() {
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = () => {
+    setTodo(input);
+    setInput("");
+  };
+  const handleDone = () => {
+    setTodo("");
+  };
+
   return (
     <main>
       <div>
         <h1>Creating Todo</h1>
         <div className="flex mt-4 w-[50rem]">
           <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
             className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
             placeholder="Add Todo"
           ></input>
-          <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">
+          <button
+            className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
+            onClick={handleSubmit}
+          >
             Add
+          </button>
+        </div>
+        <div className="p-10 flex">
+          <div className=" bg-slate-200 rounded-md w-fit p-5">
+            <h1>{todo}</h1>
+          </div>
+          <button
+            onClick={handleDone}
+            className=" bg-blue-700 text-white p-5 ml-3 rounded-md"
+          >
+            Done
           </button>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 
 export default function Home() {
+  const product = [
+    { name: "Orange", price: "17" },
+    { name: "Mango", price: "15" },
+    { name: "Banana", price: "16" },
+    { name: "Grapes", price: "18" },
+  ];
+
   return (
     <>
       <div className="">
-        <h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
-          doloremque laboriosam natus quis placeat, labore aperiam aliquid? Non,
-          earum. Libero voluptas dolores adipisci cum, ducimus suscipit ratione,
-          quia ullam possimus optio in voluptate doloribus! Minus, libero
-          officiis asperiores saepe quae quo quos impedit fuga natus molestias
-          similique mollitia ab nobis necessitatibus neque nesciunt modi
-          reiciendis velit recusandae, voluptatibus aspernatur quibusdam qui.
-          Quibusdam dicta sint quisquam repellat eveniet officia necessitatibus
-          aperiam minus ratione atque et, iusto ea! Esse suscipit, aut provident
-          ipsum voluptates unde autem impedit. Iusto rerum eligendi ipsam
-          exercitationem libero corrupti ut voluptatibus eum nam sint? Minima,
-          quaerat nam?
-        </h1>
+        {product.map((products, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-10 font-semibold bg-slate-50 group p-5"
+          >
+            <h1 className=" ">{products.name}</h1>
+            <p>{products.price}</p>
+          </div>
+        ))}
       </div>
     </>
   );
