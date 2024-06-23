0b50335d24038cd5e16fcb8182c4eb057c2dd8df
Khyle Matthew P. Yanes <yaneskhylematthew02@gmail.com>
2024-06-23 18:24:13 +0800

    Add array in create

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -3,14 +3,19 @@ import { useState } from "react";
 
 export default function Contact() {
   const [input, setInput] = useState("");
-  const [todo, setTodo] = useState("");
+  const [inputData, setInputData] = useState<string[]>([]);
 
-  const handleSubmit = () => {
-    setTodo(input);
+  const handleSubmit = (formData: FormData) => {
+    const newData = formData.get("input") as string;
+
+    setInputData((prevInputData) => [...prevInputData, newData]);
     setInput("");
   };
-  const handleDone = () => {
-    setTodo("");
+
+  const removeData = (index: number) => {
+    setInputData((prevInputData) =>
+      prevInputData.filter((_, i) => i !== index)
+    );
   };
 
   return (
@@ -18,30 +23,30 @@ export default function Contact() {
       <div>
         <h1>Creating Todo</h1>
         <div className="flex mt-4 w-[50rem]">
-          <input
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-            className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-            placeholder="Add Todo"
-          ></input>
-          <button
-            className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
-            onClick={handleSubmit}
-          >
-            Add
-          </button>
+          <form action={handleSubmit}>
+            <input
+              name="input"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+              placeholder="Add Todo"
+            ></input>
+            <button className=" p-2 border-2 rounded " type="submit">
+              Add
+            </button>
+          </form>
         </div>
-        <div className="p-10 flex">
-          <div className=" bg-slate-200 rounded-md w-fit p-5">
-            <h1>{todo}</h1>
-          </div>
-          <button
-            onClick={handleDone}
-            className=" bg-blue-700 text-white p-5 ml-3 rounded-md"
-          >
-            Done
-          </button>
-        </div>
+        <ul>
+          {inputData.map((data, index) => (
+            <li key={index} className=" bg-slate-200 p-5 mt-3 flex">
+              {data}
+              <button
+                onClick={() => removeData(index)}
+                className="p-3 bg-blue-700 text-white rounded ml-5"
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
